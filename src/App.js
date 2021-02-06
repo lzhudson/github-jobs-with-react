91ad0d5add25397e49758aba47f6841a37079b61
Hudson Holanda <hudsonholanda55@gmail.com>
2021-02-06 00:27:49 -0300

    feat: add Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import { Header } from './components/Header';
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <>
-      <h1>Hello React</h1>
+      <Header />
       <GlobalStyle />
     </>
   );
